ec7b62e676bb73116308fc1c69c182904904e0ea
Wu Jingxuan <472941837@qq.com>
2020-10-21 17:30:49 +0800

    update dataframe to html

--- python/MessageBox.py
+++ python/MessageBox.py
@@ -1,7 +1,6 @@
 from easygui import multchoicebox, msgbox
 import pandas as pd
 
-
 text = "Selected any item from the list gievn below"
 
 # window title
@@ -9,24 +8,25 @@ title = "Recipe Recommendation"
 
 # item choices
 choices = ["Healthy", "Low-fat", "Low-calorie", "Low-sodium", "Low-protein", "Low-cholesterol", "Low-sodium",
-               "Easy",
-               "Vegetarian", "5-ingredients-or-less"]
+           "Easy",
+           "Vegetarian", "5-ingredients-or-less"]
 
 # creating a multi choice box
 output = multchoicebox(text, title, choices)
-
-re = list(output)
-    # title for the message box
+print(output)
+# title for the message box
 title = "Message Box"
-
-    # message
+# message
 message = "Selected items : " + str(output)
 
+if str(output) == 'None':
+    print("You choose nothing")
+else:
+    re1 = list(output)
     # creating a message box
 msg = msgbox(message, title)
-print(re)
-# message to be displayed 
 
+# message to be displayed
 
 
 df = pd.read_csv(r'D:\NUS\Pattern Recognition Systems\Project\50000.csv', encoding='ISO-8859-1')
@@ -36,6 +36,7 @@ def rnd_select(a):
     b1 = []
     b2 = []
     b3 = []
+
     for i in range(len(df)):
         if a in df.loc[i, 'tags']:
             a1 = df.loc[i, 'name']
@@ -44,13 +45,16 @@ def rnd_select(a):
             b1.append(a1)
             b2.append(a2)
             b3.append(a3)
+
     f1 = pd.DataFrame(columns=['name', 'ingredients', 'steps'])
 
     f1['name'] = b1
     f1['ingredients'] = b2
     f1['steps'] = b3
     result = f1.sample(n=10, axis=0)
-    print(result)
+    result.to_excel('data.xls', sheet_name='data',)
+    h = result.to_html('data.html')
+    return h
 
 
 var1 = 'Healthy'
@@ -63,30 +67,29 @@ var7 = 'Easy'
 var8 = 'Vegetarian'
 var9 = '5-ingredients-or-less'
 
-
-if var1 in re:
+if var1 in re1:
     rnd_select(var1.lower())
 
-if var2 in re:
+if var2 in re1:
     rnd_select(var2.lower())
 
-if var3 in re:
+if var3 in re1:
     rnd_select(var3.lower())
 
-if var4 in re:
+if var4 in re1:
     rnd_select(var4.lower())
 
-if var5 in re:
+if var5 in re1:
     rnd_select(var5.lower())
 
-if var6 in re:
+if var6 in re1:
     rnd_select(var6.lower())
 
-if var7 in re:
+if var7 in re1:
     rnd_select(var7.lower())
 
-if var8 in re:
+if var8 in re1:
     rnd_select(var8.lower())
 
-if var9 in re():
+if var9 in re1:
     rnd_select(var9.lower())
